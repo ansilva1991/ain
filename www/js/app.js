e6aa276e243d66a3ce96478e925b979276114713
Agustín Silva <agustinsilva1991@gmail.com>
2017-06-12 17:37:47 -0300

    expenses view

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,9 @@
 var app = {
-  VERSION: 224,
+  VERSION: 225,
   PORTAL_VERSION: 224,
   MINIUM_VERSION_LOGUINED: 222,
-  ENV: "production",
-  DEV_IP: "10.0.200.123",
+  ENV: "development",
+  DEV_IP: "10.0.200.23",
   onesignal_active: false,
   load_screen_ajax: false,
   current_screen: false,
@@ -100,7 +100,10 @@ var app = {
         app.updateConfig(function(){
           app.updateMenuInfo();
           if(localStorage['welcome_' + PrivateData.get('email_logined')]){
-            app.loadScreen(app.SCREENS.DASHBOARD);
+            //app.loadScreen(app.SCREENS.DASHBOARD);
+            app.loadScreen(app.SCREENS.EXPENSES_SHOW_FULL,{
+              entry_id: 79
+            });
           }else{
             app.loadScreen(app.SCREENS.WELCOME);
           }
